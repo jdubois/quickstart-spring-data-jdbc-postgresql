8a2f723a690e04afb3f4a9e4aef435fa20c57fd8
Julien Dubois <julien.dubois@gmail.com>
2022-02-07 16:58:27 +0100

    add log4j

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class TodoController {
+
+    protected static final Logger parentLogger = LogManager.getLogger();
+
+    private Logger logger = parentLogger;
 
     private final TodoRepository todoRepository;
 
@@ -16,6 +22,7 @@ public class TodoController {
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodo(@RequestBody Todo todo) {
+        logger.debug(todo);
         return todoRepository.save(todo);
     }
 
